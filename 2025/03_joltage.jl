7e4e2351e5fb5ad737014bc114e0e7de83c421dc
Notger Heinz <notger.heinz@gmail.com>
2025-12-03 09:32:30 +0100

    Added 2025-03 in Julia.

---
+++ 2025/03_joltage.jl
@@ -0,0 +1,52 @@
+example = split("987654321111111
+811111111111119
+234234234234278
+818181911112111", '\n')
+
+puzzle = readlines("data/03.dat")
+
+function find_joltage(line, num_digits)
+    res = ""
+
+    while num_digits > 0
+        if length(line) > 1
+            digit = maximum(chop(line, head=0, tail=num_digits-1))
+            idx = findfirst(digit, line)
+        else
+            digit = line
+            idx = 1
+        end
+
+        if idx < length(line)
+            line = chop(line, head=idx, tail=0)
+        end
+        res *= digit
+        num_digits -= 1
+    end
+
+    return parse(Int, res)
+end
+
+#=
+println(find_joltage(example[1], 2))
+println(find_joltage(example[2], 2))
+println(find_joltage(example[3], 2))
+println(find_joltage(example[4], 2))
+=#
+
+
+function solve(inp, num_digits)
+    res = 0
+
+    for line in inp
+        res += find_joltage(line, num_digits)
+    end
+
+    return res
+end
+
+@assert solve(example, 2) == 357
+@assert solve(example, 12) == 3121910778619
+
+println(solve(puzzle, 2))
+println(solve(puzzle, 12))
